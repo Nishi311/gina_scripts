5e75f0c4c6a4470511e038176d2f75a1e9553718
Nishi311 <michaelnishida@gmail.com>
2021-08-18 23:13:53 -0400

    Adding in test file and getting basic code implemented to actually read in a multi-sheet excel file

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,9 @@
+from pathlib import Path
 import pandas as pd
 
+TESTING_FILES_DIR = Path(__file__).parent.parent / "test_files"
+TEST_MAP = TESTING_FILES_DIR / "test_map.xlsx"
+
 if __name__ == "__main__":
-    pass
+    sheet_dictionary = pd.read_excel(TEST_MAP, sheet_name=None)
+    blarg = "honk"
